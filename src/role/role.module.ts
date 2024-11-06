8d66c5653af90dcf6fe5b56763de79b4521e1ccf
root <root@DESKTOP-4DU1K5P>
2024-11-06 14:36:52 -0500

    Improved Role Module

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { Role } from '@/role/role.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Role])],
   providers: [RoleService],
   controllers: [RoleController],
 })
