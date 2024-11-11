79f301c130d74f53c1212f0ba02825bc587a2a6a
Luiggi Andree Arteaga Santana <115562316+Luxtar90@users.noreply.github.com>
2024-11-11 00:15:09 -0500

    Actualizar main.ts
    implementacion del swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './config/logger.config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonConfig),
+  });
+
+  // Global Pipe Config
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
+  // CORS
+  app.enableCors();
+
+  // Apply global filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Vinculation')
+    .setDescription('Documentación de la API para el proyecto Vinculation')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // La documentación estará disponible en /api
+
+  await app.listen(3000);
 }
 bootstrap();
